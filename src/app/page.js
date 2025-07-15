d16b2cc54f21299ba9b3c7b33240b5e8c07e7a6c
banuaria <banuaria@gmail.com>
2025-07-16 00:37:40 +0700

    swiperCard

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,20 @@
 
 import Navbar from '../components/Navbar';
 import Carousel from '../components/Carousel';
+import Swiper from '../components/SwiperCard';
 import Card from '../components/Card'
 import FinancialInsights from '../components/insight';
 import Footer from '@/components/footer';
+import { useRouter } from 'next/router';
 
 function Home() {  
+
   return (
     <div>
       <Navbar />
       <Carousel />
       <Card/>
+      <Swiper/>
       <FinancialInsights/>
       <Footer/>
     </div>
