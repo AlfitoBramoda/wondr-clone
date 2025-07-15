c77b24131fe31c22d62778f8b7b57efb926cdc1c
auliaanniisa <auliaanniisaa@gmail.com>
2025-07-15 16:29:52 +0700

    feat: Added feature footer 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 "use client";
 
 import { useState, useEffect } from 'react';
@@ -13,6 +14,19 @@ function Home() {
         <h1 className="text-2xl font-bold mb-6">User Dashboard (CSR)</h1>
       </div>
     </div>
+=======
+import Footer from '../components/footer';
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-center mt-10"> </h1>
+      
+      {/* Konten lain di sini */}
+
+      <Footer />
+    </main>
+>>>>>>> Stashed changes
   );
 }
 export default Home;
